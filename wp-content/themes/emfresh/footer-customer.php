3702d4f98c1b2ac37337d70d27fc42028336a0be
DESKTOP-STO6JIE\lenovo <dan@razona.jp>
2024-11-03 03:53:54 +0700

    update

--- wp-content/themes/emfresh/footer-customer.php
+++ wp-content/themes/emfresh/footer-customer.php
@@ -14,10 +14,7 @@
 </div>
 </div>
   <footer class="main-footer">
-    <div class="float-right d-none d-sm-block">
-      <b>Version</b> 1.0
-    </div>
-    <strong>Copyright &copy; 2024.</strong> All rights reserved.
+
   </footer>
   <!-- /.control-sidebar -->
 </div>
