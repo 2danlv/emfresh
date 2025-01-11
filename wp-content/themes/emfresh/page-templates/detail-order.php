7df68490dae1971dac213e46a3e7809134802475
duytmn-dev <duytmn.dev@gmail.com>
2025-01-12 03:16:36 +0700

    [ update ] Thông tin đơn hàng

--- wp-content/themes/emfresh/page-templates/detail-order.php
+++ wp-content/themes/emfresh/page-templates/detail-order.php
@@ -30,19 +30,156 @@ get_header();
 				</div>
 				<div class="card-header">
 					<ul class="nav tab-nav tabNavigation pt-16">
-						<li class="nav-item selected" rel="info">Thông tin khách hàng</li>
-						<li class="nav-item" rel="note">Ghi chú</li>
+						<li class="nav-item selected" rel="info">Thông tin đơn hàng</li>
+						<li class="nav-item" rel="note">Thanh toán</li>
 						<li class="nav-item" rel="settings-product">Chỉnh sửa thông tin</li>
 						<li class="nav-item" rel="history">Lịch sử thao tác</li>
+						<li class="nav-item" rel="reserve">Bảo lưu</li>
 					</ul>
 				</div>
 			</div>
 			<div class="card-primary">
 				<!-- Content Header (Page header) -->
-				<h1 class="pt-8">#12345</h1>
+				<div class="head-section d-f jc-b pb-16 ai-center">
+					<div class="order-code pl-16">#12345</div>
+					<div class="group-btn d-f gap-8 ai-center">
+						<div class="print d-f gap-8 ai-center"><span class="fas fas-print"></span>In đơn</div>
+						<div class="btn btn-danger remove-customer modal-button" data-target="#modal-default">
+							Xoá đơn này
+						</div>
+					</div>
+				</div>
 				<div class="card-body">
 					<div class="tab-content">
 						<div class="tab-pane" id="info">
+							<div class="row row32">
+								<div class="col-8">
+									<div class="section-wapper">
+										<div class="tlt-section">Đơn hàng</div>
+										<div class="section-content">
+											<div class="d-f ai-center jc-b">
+												<p class="txt">Phân loại đơn hàng:</p>
+												<p class="txt">W</p>
+											</div>
+											<div class="d-f ai-center jc-b">
+												<p class="txt">Mã gói sản phẩm:</p>
+												<p class="txt">1SM+1EM</p>
+											</div>
+											<div class="d-f ai-center jc-b">
+												<p class="txt">Ngày bắt đầu:</p>
+												<p class="txt">04/11/2024</p>
+											</div>
+											<div class="d-f ai-center jc-b">
+												<p class="txt">Ngày kết thúc:</p>
+												<p class="txt">-</p>
+											</div>
+										</div>
+									</div>
+									<div class="section-wapper">
+										<div class="tlt-section">Sản phẩm</div>
+										<div class="section-content">
+											<div class="product-item">
+												<div class="product-head d-f jc-b ai-center">
+													<p class="txt fw-bold">Slimfit M x <span class="quantity">5</span></p>
+													<p class="txt">325.000</p>
+												</div>
+												<div class="product-body">
+													<div class="note-txt">Note rau củ: <span>cà rốt, bí đỏ, củ dền, bí ngòi</span></div>
+													<div class="note-txt">Note tinh bột: <span>thay bún sang cơm trắng, thay miến sang cơm trắng, 1/2 tinh bột</span></div>
+													<div class="note-txt">Note khác: <span>ko rau lá, chỉ củ, 2 sốt</span></div>
+													<div class="note-txt">Note đính kèm: <span>thêm 1 tương ớt, thêm 1 ớt, túi riêng</span></div>
+												</div>
+											</div>
+											<div class="product-item">
+												<div class="product-head d-f jc-b ai-center">
+													<p class="txt fw-bold">Eatclean M x <span class="quantity">5</span></p>
+													<p class="txt">325.000</p>
+												</div>
+											</div>
+										</div>
+										<div class="section-ship line-dots-top">
+											<div class="d-f ai-center jc-b">
+												<p class="txt">Là đơn gộp tụ ship?</p>
+												<p class="txt">Không</p>
+											</div>
+											<div class="d-f ai-center jc-b">
+												<p class="txt">Số ngày phát sinh phí ship:</p>
+												<p class="txt">5</p>
+											</div>
+											<div class="d-f ai-center jc-b">
+												<p class="txt">Tổng tiền phí ship:</p>
+												<p class="txt">50.000</p>
+											</div>
+											<div class="d-f ai-center jc-b">
+												<p class="txt">Giảm giá:</p>
+												<p class="txt">-</p>
+											</div>
+											<div class="d-f ai-center jc-b mt-4">
+												<p class="txt black fw-bold">Tổng tiền đơn hàng:</p>
+												<p class="cost-txt">700.000</p>
+											</div>
+										</div>
+									</div>
+									<div class="section-wapper">
+										<div class="tlt-section">Thanh toán</div>
+										<div class="section-content">
+											<div class="d-f ai-center jc-b">
+												<p class="txt">Cần thanh toán:</p>
+												<p class="txt">700.000</p>
+											</div>
+											<div class="d-f ai-center jc-b">
+												<p class="txt">Đã thanh toán:</p>
+												<p class="txt">200.000</p>
+											</div>
+										</div>
+										<div class="section-ship line-dots-top">
+											<div class="d-f ai-center jc-b mt-4">
+												<p class="txt black fw-bold">Số tiền còn lại:</p>
+												<p class="cost-txt red">500.000</p>
+											</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-4">
+									<div class="section-wapper">
+										<div class="tlt-section">Khách hàng</div>
+										<div class="section-content">
+											<p class="txt">Linh (Nu Kenny)</p>
+											<p class="txt blue">0123456789</p>
+											<p class="txt">44L đường số 11, KDC Miếu Nổi, Phường 3, Quận Bình Thạnh</p>
+											<p class="note-txt italic">(Đã đăng ký chung nhóm ship: Thien Phuong Bui)</p>
+										</div>
+									</div>
+									<div class="section-wapper">
+										<div class="tlt-section">Trạng thái</div>
+										<div class="section-content status-content">
+											<div class="d-f jc-b ai-center">
+												<p class="txt">Trạng thái đặt đơn:</p>
+												<div class="tag-status purple">Dí món</div>
+											</div>
+											<div class="d-f jc-b ai-center">
+												<p class="txt">Phương thức thanh toán:</p>
+												<p class="txt">Chuyển khoản</p>
+											</div>
+											<div class="d-f jc-b ai-center">
+												<p class="txt">Trạng thái thanh toán:</p>
+												<div class="tag-status purple">1 phần</div>
+											</div>
+											<div class="d-f jc-b ai-center">
+												<p class="txt">Trạng thái đơn hàng:</p>
+												<div class="tag-status green">Đang dùng</div>
+											</div>
+										</div>
+									</div>
+									<div class="section-wapper">
+										<div class="tlt-section">Giao hàng</div>
+										<div class="section-content">
+											<p class="txt black ellipsis">Thứ 3 (06/01): 45 Hoa Lan, Phường 3, Quận Phú Nhuận</p>
+											<p class="note-txt italic">(Đã đăng ký chung nhóm ship: Thien Phuong Bui)</p>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
 						<!-- /.tab-pane -->
 						<div class="tab-pane" id="note">
@@ -204,6 +341,8 @@ get_header();
 						</div>
 						<div class="tab-pane" id="history">
 						</div>
+						<div class="tab-pane" id="reserve">
+						</div>
 						<!-- /.tab-pane -->
 					</div>
 					<!-- /.tab-content -->
@@ -215,6 +354,10 @@ get_header();
 		</div>
 
 </div><!-- /.container-fluid -->
+<div class="navigation-bottom d-f jc-b ai-center pl-16 pr-16">
+	<div class="total-cost txt d-f gap-16 ai-center fw-bold">Cần thanh toán: <span class="cost-txt red fw-bold">500.000</span></div>
+	<span class="btn btn-primary btn-next">Đi đến Meal Plan chi tiết</span>
+</div>
 </section>
 <!-- /.content -->
 </div>
@@ -225,7 +368,7 @@ get_header();
 		<div class="modal-content">
 			<form method="post" id="list-customer" action="<?php the_permalink() ?>">
 				<div class="modal-body pt-8 pb-16">
-					<input type="hidden" class="customer_id" name="customer_id" value="<?php echo $response_customer['data']['id'] ?>">
+					<input type="hidden" class="customer_id" name="customer_id" value="">
 					<div class="d-f ai-center">
 						<i class="fas fa-warning mr-4"></i>
 						<p>Bạn có chắc muốn xoá khách hàng này không?</p>
