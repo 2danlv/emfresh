339bd8815b7901200add4d4e35c0950c703497ff
DESKTOP-STO6JIE\lenovo <dan@yahoo.jp>
2025-01-21 18:19:14 +0700

    chia file

---
+++ wp-content/themes/emfresh/parts/order/edit-detail-pay.php
@@ -0,0 +1,69 @@
+<div class="card">
+    <div class="total-pay d-f jc-b ai-center">
+        <p>Tổng tiền sản phẩm:</p>
+        <p class="price-product fw-bold"><?php echo $order_detail['total_amount'] > 0 ? number_format($order_detail['total_amount']) : 0; ?></p>
+        <input type="hidden" name="total_amount" class="input-total_amount" value="<?php echo $order_detail['total_amount'] ?>" />
+    </div>
+    <div class="shipping-fee">
+        <div class="fee-item d-f jc-b ai-center">
+            <p>Là đơn gộp tụ ship?</p>
+            <div class="d-f gap-12 ai-center">
+                <label class="auto-fill-checkbox">
+                    <input type="checkbox">
+                    <span class="slider"></span>
+                </label>
+                <span class="fs-16 color-gray">Không</span>
+            </div>
+        </div>
+        <div class="fee-item d-f jc-b ai-center">
+            <p>Số ngày phát sinh phí ship:</p>
+            <input type="number" name="ship_days" class="input-ship_days" value="<?php echo $order_detail['ship_days'] ?>" />
+        </div>
+        <div class="fee-item d-f jc-b ai-center">
+            <p>Tổng tiền phí ship:</p>
+            <input type="number" name="ship_amount" class="input-ship_amount" value="<?php echo $order_detail['ship_amount'] ?>" />
+        </div>
+        <div class="fee-item d-f jc-b ai-center">
+            <p>Giảm giá:</p>
+            <input type="number" name="discount" class="input-discount" value="<?php echo $order_detail['discount'] ?>" />
+        </div>
+    </div>
+    <div class="total-pay d-f jc-b ai-center">
+        <p>Tổng tiền đơn hàng:</p>
+        <p class="price-order fw-bold"><?php echo ($total = $order_detail['total_amount'] + $order_detail['ship_amount']) > 0 ? number_format($total) : 0; ?></p>
+    </div>
+    <div class="order-payment">
+        <div class="payment-item d-f jc-b ai-center">
+            <p>Phương thức thanh toán:</p>
+            <div class="d-f jc-b ai-center gap-16">
+                <?php
+                foreach ($list_payment_methods as $value => $label) {
+                    printf('<label class="d-f ai-center gap-8"><input type="radio" name="payment_method" value="%s" %s>%s</label>', $value, $order_detail['payment_method'] == $value ? 'checked' : '', $label);
+                }
+                ?>
+            </div>
+        </div>
+        <div class="payment-item d-f jc-b ai-center">
+            <p>Trạng thái thanh toán:</p>
+            <div class="status-payment">
+                <div class="status-pay"><span class="red">Chưa</span></div>
+                <input type="hidden" name="payment_status" class="payment_status input_status-payment" value="" />
+                <ul class="status-pay-menu">
+                    <?php
+                    foreach ($list_payment_statuses as $value => $label) {
+                        printf('<li class="status-pay-item"><span data-status="%d" %s>%s</span></li>', $value, $order_detail['payment_status'] == $value ? 'selected' : '', $label);
+                    }
+                    ?>
+                </ul>
+            </div>
+        </div>
+        <div class="paymented d-f jc-b ai-center pt-8">
+            <p>Đã thanh toán:</p>
+            <input type="number" name="number" placeholder="-" class="form-control text-right">
+        </div>
+        <div class="payment-item d-f jc-b ai-center pt-8">
+            <p>Cần thanh toán:</p>
+            <div class="payment-required fw-bold"> <?php echo number_format($order_detail['total_amount'] + $order_detail['ship_amount']) ?></div>
+        </div>
+    </div>
+</div>
---
+++ wp-content/themes/emfresh/parts/order/edit-detail-ship.php
@@ -0,0 +1,69 @@
+<div class="card">
+    <div class="pl-16 pr-16">
+        <div class="row delivery-item">
+            <div class="col-4">Đặt lịch:</div>
+            <div class="col-8">
+                <label for="loop" class="d-f ai-center gap-12 pb-8 loop">
+                    <input type="checkbox" name="loop" id="loop">
+                    Lặp lại hàng tuần
+                </label>
+                <div class="calendar">
+                    <input type="text" name="calendar" placeholder="DD/MM/YYYY" class="form-control js-calendar date">
+                </div>
+                <div class="repeat-weekly">
+                    <input type="checkbox" id="monday" hidden name="days" value="monday">
+                    <label for="monday">Thứ Hai</label>
+                    <input type="checkbox" id="tuesday" hidden name="days" value="tuesday">
+                    <label for="tuesday"> Thứ Ba</label>
+                    <input type="checkbox" id="wednesday" hidden name="days" value="wednesday">
+                    <label for="wednesday"> Thứ Tư</label>
+                    <input type="checkbox" id="thursday" hidden name="days" value="thursday">
+                    <label for="thursday"> Thứ Năm</label>
+                    <input type="checkbox" id="friday" hidden name="days" value="friday">
+                    <label for="friday"> Thứ Sáu</label>
+                </div>
+            </div>
+        </div>
+        <div class="row delivery-item pt-24 ai-center">
+            <div class="col-4">Địa chỉ giao:</div>
+            <div class="col-8 address">
+                <div class="dropdown">
+                    <select name="ship_location_id" class="form-control select-location_id input-location_id">
+                        <?php
+                        foreach ($list_locations as $location) {
+                            printf('<option value="%s" %s>%s</option>', $location['id'], 0 == $location['id'] ? 'selected' : '', $location['location_name']);
+                        }
+                        ?>
+                    </select>
+                    <span class="fs-14 hidden fw-regular note-shipper color-gray pl-8">Note với shipper: <span class="note_shiper"></span></span>
+                </div>
+                <div class="dropdown-menu">
+                    <div class="item">
+                        <p class="fs-16 color-black other-address">44L đường số 11, KDC Miếu Nổi, Phường 3, Quận Bình Thạnh</p>
+                        <p class="note_shiper hidden">gửi lễ tân/bảo vệ rồi nhắn tin khách</p>
+                    </div>
+                    <a href="#modal-add-address" class="btn-add-address d-f ai-center pb-16 pt-8 pl-8">
+                        <span class="fas fa-plus mr-8"></span>Thêm địa chỉ mới
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="js-note delivery-item">
+            <div class="row pt-16 ai-center">
+                <div class="col-4">Note shipper theo ngày:</div>
+                <div class="col-8">
+                    <input type="text" name="note_shipper_by_day" class="form-control note_shipper_by_day">
+                </div>
+            </div>
+            <div class="row pt-16 ai-center">
+                <div class="col-4">Note admin theo ngày:</div>
+                <div class="col-8">
+                    <input type="text" name="note_admin_by_day" class="form-control note_admin_by_day">
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="d-f ai-center pb-16 pt-24 add-new-note">
+    <span class="fas fa-plus mr-8"></span> Thêm note giao hàng mới
+</div>
